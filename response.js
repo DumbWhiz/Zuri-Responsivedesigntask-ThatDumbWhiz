f9c5823cf3f6569850cfc3ee45b33fff5f61bdb1
DumbWhiz <adeyeyewilson@gmail.com>
2022-07-31 05:13:38 +0100

    Responsive design modification
    Responsive design

--- response.js
+++ response.js
@@ -1,61 +1,61 @@
- const form = document.getElementById('form');
- const firstname = document.getElementById('firstname');
- const lastname = document.getElementById('lastname');
- const email = document.getElementById('email');
- const password = document.getElementById('pass');
+const form = document.getElementById('form');
+const firstname = document.getElementById('firstname');
+const lastname = document.getElementById('lastname');
+const email = document.getElementById('email');
+const password = document.getElementById('pass');
 
- form.addEventListener('submit', (e) => {
-    e.preventDefault();
+form.addEventListener('submit', (e) => {
+   e.preventDefault();
 
-    checkInputs();
- });
+   checkInputs();
+});
 
- function checkInputs(){
-    const firstnameValue = firstname.value.trim();
-    const lastnameValue = lastname.value.trim();
-    const emailValue = email.value.trim();
-    const passwordValue = password.value.trim();
- 
+function checkInputs() {
+   const firstnameValue = firstname.value.trim();
+   const lastnameValue = lastname.value.trim();
+   const emailValue = email.value.trim();
+   const passwordValue = password.value.trim();
 
- if(firstnameValue === ''){
-    //show error
-    //add error class
-    setErrorfor(firstname, 'firstname can not be empty');
- } else {
-    //add success class
-    setSuccessfor(firstname);
-}
 
-   if (lastnameValue === ''){
+   if (firstnameValue === '') {
+      //show error
+      //add error class
+      setErrorfor(firstname, 'firstname can not be empty');
+   } else {
+      //add success class
+      setSuccessfor(firstname);
+   }
+
+   if (lastnameValue === '') {
       setErrorfor(lastname, 'lastname cannot be empty');
-      }      else {
-         setSuccessfor(lastname);
-      }
-   
-      if (emailValue === ''){
-         setErrorfor(email, 'email cannot be empty');
-         } else   if(!isEmail(emailValue)){
-            setErrorfor(email, 'Looks like this is not an email');
-         }  else{
-            setSuccessfor(email);
-         }
-   
-      if (passwordValue === ''){
-         setErrorfor(password, 'password cannot be empty');
-         }      
-         else {
-            setSuccessfor(password);
-         }
- }
-function setErrorfor(input, message){
-const detailBox = input.parentElement; //detail-box
-const small = detailBox.querySelector('small');
+   } else {
+      setSuccessfor(lastname);
+   }
 
-//small error message
-small.innerText = message;
+   if (emailValue === '') {
+      setErrorfor(email, 'email cannot be empty');
+   } else if (!isEmail(emailValue)) {
+      setErrorfor(email, 'Looks like this is not an email');
+   } else {
+      setSuccessfor(email);
+   }
 
-//error class
-detailBox.className = 'detail-box error'
+   if (passwordValue === '') {
+      setErrorfor(password, 'password cannot be empty');
+   }
+   else {
+      setSuccessfor(password);
+   }
+}
+function setErrorfor(input, message) {
+   const detailBox = input.parentElement; //detail-box
+   const small = detailBox.querySelector('small');
+
+   //small error message
+   small.innerText = message;
+
+   //error class
+   detailBox.className = 'detail-box error'
 }
 
 function setSuccessfor(input, message) {
@@ -64,5 +64,5 @@ function setSuccessfor(input, message) {
 }
 
 function isEmail(email) {
-   return/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email);
+   return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email);
 }
